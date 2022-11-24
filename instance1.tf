1037988deefcfe8ba327d3807f2badaa84a3d31e
split <a@a.com>
2022-11-24 21:10:43 +0200

    changes to different tf files after instance chang

--- instance1.tf
+++ instance1.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "ins1" {
-  ami                    = "ami-08c40ec9ead489470"
+  ami                    = "ami-08c40ec9ead489470terr"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "Ubuntu2"
