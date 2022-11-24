535acfc20043a2c518d39b2d8b550f8854f2552a
split <a@a.com>
2022-11-24 20:59:30 +0200

    changed Ami type

--- instance1.tf
+++ instance1.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "ins1" {
-  ami                    = "ami-0b0dcb5067f052a63"
+  ami                    = "ami-08c40ec9ead489470"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "Ubuntu2"
