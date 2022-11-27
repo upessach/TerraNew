598222cccc8ef137fd8bfbef5d26f1d18cfd691b
split <a@a.com>
2022-11-27 22:39:42 +0200

    added vnet to main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,17 @@ provider "azurerm" {
   features {}
 }
 
+# Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "centralus"
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.10.0.0/16"]
+  location            = "centralus"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
